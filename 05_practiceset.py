0d6b06b23d81999886132285b6bcfd60b96feae6
rishi048229 <rishisingh048229@gmail.com>
2025-09-25 12:41:54 +0530

    new program

---
+++ 05_practiceset.py
@@ -0,0 +1,9 @@
+l= ["RAM" , "sita" , "shyam" , "shiv"]
+
+name = input("Enter your name: ")
+
+if(name in l):
+    print("Your name is in the list")
+
+else:
+    print("Your name is not in the list")
